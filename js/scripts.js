1b085d3fad75381028f2b64b8d070467e8a1eef9
Pragun Bhutani <pragun06@gmail.com>
2013-03-31 18:04:31 +0530

    Testing basic animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 function insertImage()	{
 
-	var newHTML = "<div id='image-bg'><img src='" + document.getElementById('imageURL').value + "' /></div>";
+	var newHTML = "<div id='image-bg'><img id='userImg' src='" + document.getElementById('imageURL').value + "' /></div>";
 	document.getElementById('image-container').innerHTML = newHTML;
 	
+	$('#image-bg').hide();	
+	$('#userImg').hide();
+
+	$('#image-bg').fadeIn('fast');
+	
+	$('#userImg').bind("load", function() {
+		$(this).fadeIn('slow');
+	});
+
 };
