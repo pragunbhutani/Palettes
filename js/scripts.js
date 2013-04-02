70053029b1ca26b4435bd763cacf70ca65d86e45
Pragun Bhutani <pragun06@gmail.com>
2013-04-03 01:24:28 +0530

    Palette display fix #7

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ function insertImage()	{
 	$('#userImg').hide();
 	$(".swatch-box").hide();
 
+
+	$('image-container').fadeIn('fast', function() {
+		$(this).css("border", "1px solid");
+	});
 	$('#image-bg').fadeIn('fast');
 	
 	$('#userImg').bind("load", function() {
